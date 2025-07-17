c16e601f7ac31b7e15d04721ed867a03aa912378
khayamt <khayam@k-polit.co.za>
2025-07-18 00:03:00 +0200

    Initial commit

---
+++ src/main/java/co/za/k_polit/gateway_service/logging/LoggingFilter.java
@@ -0,0 +1,31 @@
+package co.za.k_polit.gateway_service.logging;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.cloud.gateway.filter.GatewayFilterChain;
+import org.springframework.cloud.gateway.filter.GlobalFilter;
+import org.springframework.core.Ordered;
+import org.springframework.stereotype.Component;
+import org.springframework.web.server.ServerWebExchange;
+import reactor.core.publisher.Mono;
+
+@Component
+public class LoggingFilter implements GlobalFilter, Ordered {
+    private static final Logger logger = LoggerFactory.getLogger(LoggingFilter.class);
+    public LoggingFilter() {
+        logger.info(">>> LoggingFilter bean initialized");
+    }
+    @Override
+    public Mono<Void> filter(ServerWebExchange exchange, GatewayFilterChain chain)
+    {
+        logger.info("Incoming Request: {} {}", exchange.getRequest().getMethod(), exchange.getRequest().getURI());
+        return chain.filter(exchange).then(Mono.fromRunnable(() -> {
+            logger.info("Outgoing Response: {}", exchange.getResponse().getStatusCode());
+        }));
+    }
+    @Override
+    public int getOrder()
+    {
+        return -1;
+    }
+}
---
+++ src/test/java/co/za/k_polit/gateway_service/GatewayServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package co.za.k_polit.gateway_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GatewayServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
